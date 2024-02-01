26cea73d063816b0719a6ae35a9e08e31df6fcf5
yannis.doumir_fernandes <yannis.doumir_fernandes@etuc.uca.fr>
2024-02-01 13:22:26 +0100

    ADD code

---
+++ js/index.js
@@ -0,0 +1,4 @@
+function toggleLanguageDropdown() {
+  var dropdown = document.getElementById("languageDropdown");
+  dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+}
