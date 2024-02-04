eded663a9234487a623b1a596dc7a8a5b8fb1ff0
yannis.doumir_fernandes <yannis.doumir_fernandes@etuc.uca.fr>
2024-02-04 22:19:37 +0100

    ADD navbar fonctionnel + new color (maybe i will change it later) + js for de part "aPropos"

--- js/aPropos.js
+++ js/aPropos.js
@@ -3,3 +3,30 @@ var p_Langue = document.getElementsByClassName(".para_Langue");
 var p_SoftSkills = document.getElementsByClassName(".para_SoftSkills");
 var p_Passions = document.getElementsByClassName(".para_Passions");
 
+function masquerTousLesParagraphes() {
+    var tousLesParagraphes = document.querySelectorAll('.aPropos_Contenu p');
+    tousLesParagraphes.forEach(function(paragraphe) {
+        paragraphe.style.display = "none";
+    });
+}
+
+function afficherContenu(classe) {
+    masquerTousLesParagraphes(); // Masque tous les paragraphes avant d'afficher le spécifique
+    document.querySelector(classe + ' p').style.display = "block";
+}
+
+function txtPresentation() {
+    afficherContenu('.aPropos_Presentation');
+}
+
+function txtLangue() {
+    afficherContenu('.aPropos_Langue');
+}
+
+function txtSoftSkills() {
+    afficherContenu('.aPropos_SoftSkills');
+}
+
+function txtPassions() {
+    afficherContenu('.aPropos_Passions');
+}
