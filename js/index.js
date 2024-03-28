0e863917562f360eb4ba7c3c03e835d7259a8966
yannis.doumir_fernandes <yannis.doumir_fernandes@etuc.uca.fr>
2024-03-28 13:49:27 +0100

    ADD

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    cosole.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+    else {
+        entry.target.classList.remove('show');
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((element) => observer.observe(element));
+
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "navbar") {
@@ -6,4 +21,20 @@ function myFunction() {
     x.className = "navbar";
   }
 }
+
+const header = document.querySelector('.navbar');
+
+function checkIfSticky() {
+  const fromTop = window.scrollY;
+  const headerHeight = header.offsetHeight;
+
+  if (fromTop >= headerHeight) {
+    header.classList.add('is-sticky');
+  } else {
+    header.classList.remove('is-sticky');
+  }
+}
+
+
+window.addEventListener('scroll', checkIfSticky);
 
